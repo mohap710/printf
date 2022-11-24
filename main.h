844855eb8ceb400043aa21544a6e8a0bd190f12a
mohap710 <mohap710@gmail.com>
2022-11-24 14:47:57 +0200

    Compiler error Fixed.

--- main.h
+++ main.h
@@ -32,7 +32,9 @@ int print_binary(va_list);
 
 /*Helper functions*/
 unsigned int base_len(unsigned int, int);
+char *rev_string(char *);
 void write_base(char *str);
+char *_memcpy(char *dest, char *src, unsigned int n);
 int print_unsgined_number(unsigned int);
 
 #endif
